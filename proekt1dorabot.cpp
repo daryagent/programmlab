d4f764de99bd400f44b837fa50807c601d20e932
daryagent <90250480+daryagent@users.noreply.github.com>
2021-09-12 20:56:52 +0500

    Add files via upload
    Добавлены вывод произведения и частного введенных чисел.

---
+++ proekt1dorabot.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+
+	float num1 = 0;
+	float num2 = 0;
+
+	cout << "Vvedi pervoe chislo: ";
+	cin >> num1;
+	cout << "Vvedi vtoroe chislo: ";
+	cin >> num2;
+
+	cout << num1 << " + " << num2 << " = " << num1 + num2 << endl;
+	cout << num1 << " - " << num2 << " = " << num1 - num2 << endl;
+
+	cout << num1 << " * " << num2 << " = " << num1 * num2 << endl;
+	cout << num1 << " / " << num2 << " = " << num1 / num2 << endl;
+
+	cout << "Sredne arifmeticheskoe: ";
+	cout << num1 << " + " << num2 << " / " << 2 << " = ";
+	cout << (num1 + num2) / 2 << endl;
+
+	cout << endl;
+
+	return 0;
+}
